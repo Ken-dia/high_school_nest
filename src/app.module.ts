b41feed46c4ce49bb0858cc9cbe8b7a5b6a92249
ken-dia <diahamidou10@gmail.com>
2022-07-31 13:54:10 +0000

    configuration mongoose et graphql validé

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TestResolver } from './app.test.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TestResolver],
 })
 export class AppModule {}
